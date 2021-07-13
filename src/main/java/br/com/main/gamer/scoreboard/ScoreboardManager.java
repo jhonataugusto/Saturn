fe2356bdec25572bf747134ad513287e4bea6fe1
Chateado <chateadorn@gmail.com>
2021-07-13 01:24:30 -0300

    Adicionando classe principal para management da scoreboard

---
+++ src/main/java/br/com/main/gamer/scoreboard/ScoreboardManager.java
@@ -0,0 +1,42 @@
+package br.com.main.gamer.scoreboard;
+
+import br.com.main.Saturn;
+import br.com.main.gamer.Gamer;
+import br.com.main.scoreboard.sidebar.Sidebar;
+import br.com.main.task.event.UpdateEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class ScoreboardManager implements Listener {
+
+    public void updateScoreboard(Player player) {
+        if (player == null) {
+            return;
+        }
+        Gamer gamer = Saturn.getInstance().getGamerManager().getGamer(player.getUniqueId());
+        Sidebar sidebar = gamer.getWrapper().getSidebar();
+
+        if (sidebar.isHided()) {
+            return;
+        }
+        sidebar.setTitle("§6§lSATURN");
+        sidebar.updateRows(rows -> {
+            rows.add(" ");
+            rows.add("§7Jogadores: §3" + Bukkit.getOnlinePlayers().size());
+            rows.add(" ");
+            rows.add("§e§owww.saturn.com.br");
+        });
+    }
+
+    @EventHandler
+    public void onUpdate(UpdateEvent event) {
+        if (event.getType() != UpdateEvent.UpdateType.SECOND) {
+            return;
+        }
+        for (Player o : Bukkit.getOnlinePlayers()) {
+            updateScoreboard(o);
+        }
+    }
+}
