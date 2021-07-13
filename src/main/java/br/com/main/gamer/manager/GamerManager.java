7139e6e3ad82d0422d295b3229b7043e48d38136
Chateado <chateadorn@gmail.com>
2021-07-13 01:24:37 -0300

    Adicionando gamer manager para os gamers

---
+++ src/main/java/br/com/main/gamer/manager/GamerManager.java
@@ -0,0 +1,32 @@
+package br.com.main.gamer.manager;
+
+import br.com.main.gamer.Gamer;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class GamerManager {
+
+    private final HashMap<UUID, Gamer> gamers;
+
+    public GamerManager() {
+        gamers = new HashMap<>();
+    }
+
+    public void loadGamer(Gamer gamer) {
+        gamers.put(gamer.getUniqueId(), gamer);
+    }
+
+    public void unloadGamer(UUID uniqueId) {
+        gamers.remove(uniqueId);
+    }
+
+    public Collection<Gamer> getGamers() {
+        return gamers.values();
+    }
+
+    public Gamer getGamer(UUID uniqueId) {
+        return gamers.get(uniqueId);
+    }
+}
